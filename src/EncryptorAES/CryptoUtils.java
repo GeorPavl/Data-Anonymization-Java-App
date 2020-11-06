b99d89c2ca0a250a0402a0d5e95fb657f3f8c637
GeorPavl <georpavloglou@gmail.com>
2020-11-06 13:47:43 +0200

    "Added h1 tag"

---
+++ src/EncryptorAES/CryptoUtils.java
@@ -0,0 +1,74 @@
+package EncryptorAES;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CryptoUtils{
+
+    // Initial Value, it is random bytes, typically 12 bytes or 16 bytes.
+    // In Java we can use SecureRandom to generate the random IV
+    public static byte[] getRandomBytes(int numberOfBytes){
+        byte[] randomBytes = new byte[numberOfBytes];
+        new SecureRandom().nextBytes(randomBytes);
+        return randomBytes;
+    }
+
+    // The AES secret key, either AES-128 or AES-256.
+    // In Java, we can use KeyGenerator to generate the AES secret key.
+    public static SecretKey getAESKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256, SecureRandom.getInstanceStrong());
+        return keyGenerator.generateKey();
+    }
+
+    // Overloaded method getAESKey.
+    // Excepts one extra parameter keySize (128 or 256)
+    public static SecretKey getAESKey(int keySize) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(keySize, SecureRandom.getInstanceStrong());
+        return keyGenerator.generateKey();
+    }
+
+    // The AES secret key that derived from a given password.
+    // In Java, we can use the SecretKeyFactory and PBKDF2WithHmacSHA256 to generate an AES key from a given password.
+    // Password derived AES 256 bits secret key
+    public static SecretKey getAESKeyFromPassword(char[] password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password, salt, 65536,256);
+        SecretKey secretKey = new SecretKeySpec(factory.generateSecret(spec).getEncoded(),"AES");
+        return secretKey;
+    }
+
+    // hex representation
+    public static String hex(byte[] bytes){
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes){
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    // print hex with block size split
+    public static String hexWithBlockSize(byte[] bytes, int blockSize){
+        String hex = hex(bytes);
+        // one hex = 2 chars;
+        blockSize = blockSize * 2;
+
+        List<String> result = new ArrayList<>();
+        int index = 0;
+        while (index < hex.length()){
+            result.add(hex.substring(index, Math.min(index + blockSize, hex.length())));
+            index += blockSize;
+        }
+        return result.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,106 +1,26 @@
-import EncryptionASE.CryptoUtils;
-import EncryptionASE.EncryptorAesGcm;
-import EncryptionASE.File;
+import File.MyFile;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
-    private static final int AES_KEY_BIT = 256;
-    private static final int IV_LENGTH_BYTE = 12;
-    private static final Charset UTF_8 = StandardCharsets.UTF_8;
-
     public static void main(String[] args) throws Exception {
 
-        // prints welcome message
-        printWelcomeMessage();
-        // creates file for encryption
-        File configFile = createFile();
-        configFile.printInList();
+        // prints welcome page
+        AppUtils.printWelcomeMessage();
 
-        System.out.println("-------");
+        // read config file
+        MyFile configFile = AppUtils.inputConfigFile();
 
-        File datasetFile = createFile();
-        datasetFile.printInList();
+        // read dataset file
+        MyFile datasetFile = AppUtils.inputDatasetFile();
 
-        List<String> list = configFile.getFileInList();
+        // encrypt columns and store in List<String>
+        List<String> encryptedFileInList = AppUtils.encryptFile(configFile,datasetFile);
+        // print encrypted list
+        AppUtils.printList(encryptedFileInList);
 
-        List<Integer> listOfColIndexes = datasetFile.findColumns(list);
-
-        List<String> encryptedList = datasetFile.encryptColumns(listOfColIndexes);
-
-        for (int i=0; i<encryptedList.size(); i++){
-            System.out.println(encryptedList.get(i));
-        }
-
-
-
-
-        /*// encrypt and decrypt need the same key.
-        // get AES 256 bits (32 bytes) key
-        SecretKey secretKey = CryptoUtils.getAESKey(AES_KEY_BIT);
-
-        // encrypt and decrypt need the same IV.
-        // AES-GCM needs IV 96-bit (12 bytes)
-        byte[] iv = CryptoUtils.getRandomBytes(IV_LENGTH_BYTE);
-        for (int i=0; i<bytesInList.size(); i++){
-            System.out.println(CryptoUtils.hex(EncryptorAesGcm.encryptWithPrefixIV(bytesInList.get(i),secretKey,iv)));
-
-        }
-        */
-    }
-    // scanner
-    private static final Scanner scanner = new Scanner(System.in);
-    // welcome message
-    public static void printWelcomeMessage(){
-        System.out.println("\n------ Welcome to EncryptorAES-GCM ------" +
-                           "\n-----------------------------------------\n");
-    }
-
-    // create file
-    private static File createFile(){
-        System.out.println("\n------ CREATE FILE ------" +
-                           "\n--------------------------\n");
-        System.out.println("Please enter the file's name you want to encrypt:");
-        String fileName = scanner.nextLine();
-        Path filePath = Paths.get(System.getProperty("user.dir") + "\\src\\" + fileName);
-        File file = new File(fileName,filePath);
-
-        System.out.println("File name: \n\t" + file.getFileName());
-        System.out.println("File path: \n\t" + file.getFilePath());
-
-        return file;
-    }
-
-    //
-    private static List<byte[]> encryptByteList(List<byte[]> originalByteList) throws Exception {
-        // encrypt and decrypt need the same key.
-        // get AES 256 bits (32 bytes) key
-        SecretKey secretKey = CryptoUtils.getAESKey(AES_KEY_BIT);
-
-        // encrypt and decrypt need the same IV.
-        // AES-GCM needs IV 96-bit (12 bytes)
-        byte[] iv = CryptoUtils.getRandomBytes(IV_LENGTH_BYTE);
-
-        //
-        System.out.println("\n------ ENCRYPT BYTE LIST ------" +
-                           "\n--------------------------\n");
-
-        System.out.println("Original string's size in bytes for encryption: " + originalByteList.size());
-
-        List<byte[]> encryptedByteList = new ArrayList<byte[]>();
-
-        for (int i=0; i<originalByteList.size(); i++){
-            System.out.println(originalByteList.get(i));
-            encryptedByteList.add(i,EncryptorAesGcm.encryptWithPrefixIV(originalByteList.get(i),secretKey,iv));
-        }
-        return encryptedByteList;
+        // create new .txt file with encrypted data
+        AppUtils.createNewFile(encryptedFileInList);
     }
 }
